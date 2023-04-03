528afb0123a5d66eb09fb060690bbe2522ec6ed8
adebemaro <adebemario@gmail.com>
2023-04-03 05:54:15 -0700

    image lessons

--- lib/main.dart
+++ lib/main.dart
@@ -10,215 +10,161 @@ class MovieApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: SafeArea(
           child: Scaffold(
+        backgroundColor: Colors.black26,
         appBar: AppBar(
-          title: const Text("Netflix App"),
-          centerTitle: true,
-          actions: const [Icon(Icons.notifications)],
+          title: const Text("Learn German"),
+          actions: const [
+            Padding(
+              padding: EdgeInsets.only(right: 8.0),
+              child: Icon(
+                Icons.search,
+                size: 30,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: 8.0),
+              child: CircleAvatar(
+                backgroundColor: Colors.blueAccent,
+                backgroundImage: AssetImage('assets/images/profile.jpeg'),
+              ),
+            )
+          ],
+          backgroundColor: Colors.grey,
         ),
         drawer: const Drawer(),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                height: 100,
-                width: 200,
-                color: Colors.blue,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 100,
-                width: 200,
-                color: Colors.green,
-              ),
-              const Text(
-                "Types of colors",
-                style: TextStyle(fontSize: 30),
-              ),
-              Container(
-                  height: 100,
-                  width: 200,
-                  color: Colors.red,
-                  child: const Center(
-                    child: Text(
-                      "This is color red",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  )),
-              const SizedBox(
-                height: 10,
-              ),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
+        body: Column(
+          children: [
+            const SizedBox(height: 30),
+            Row(
+              children: [
+                Column(
                   children: [
-                    const Text(
-                      "Row1 Colors",
-                      style: TextStyle(fontSize: 20),
-                    ),
                     Container(
-                      height: 50,
-                      width: 50,
-                      color: Colors.amber,
-                      margin: const EdgeInsets.only(left: 20, right: 20),
+                      margin: const EdgeInsets.only(left: 5),
+                      height: 60,
+                      width: 60,
+                      decoration: BoxDecoration(
+                        color: Colors.teal,
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      child: const Icon(
+                        Icons.alarm,
+                        size: 30,
+                        color: Colors.white,
+                      ),
                     ),
+                    const SizedBox(height: 10),
                     Container(
-                      height: 50,
-                      width: 50,
-                      color: Colors.purple,
-                      margin: const EdgeInsets.only(right: 20),
-                    ),
-                    Container(
-                      height: 50,
-                      width: 50,
-                      color: Colors.teal,
-                      margin: const EdgeInsets.only(right: 20),
-                    ),
-                    Container(
-                      height: 50,
-                      width: 50,
-                      color: Colors.purple,
-                      margin: const EdgeInsets.only(right: 20),
-                    ),
-                    Container(
-                      height: 50,
-                      width: 50,
-                      color: Colors.teal,
-                      margin: const EdgeInsets.only(right: 20),
-                    ),
-                    Container(
-                      height: 50,
-                      width: 50,
-                      color: Colors.amber,
-                      margin: const EdgeInsets.only(right: 20),
+                      margin: const EdgeInsets.only(left: 5),
+                      width: 100,
+                      height: 40,
+                      child: const Text(
+                        "Time and Date",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                        ),
+                      ),
                     ),
                   ],
                 ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
+                const Spacer(),
+                Column(
                   children: [
                     Container(
-                      height: 50,
-                      width: 50,
-                      color: Colors.amber,
+                      margin: const EdgeInsets.only(left: 5),
+                      height: 60,
+                      width: 60,
+                      decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      child: const Icon(
+                        Icons.map,
+                        size: 30,
+                        color: Colors.white,
+                      ),
                     ),
+                    const SizedBox(height: 10),
                     Container(
-                      height: 50,
-                      width: 50,
-                      color: Colors.purple,
-                    ),
-                    Container(
-                      height: 50,
-                      width: 50,
-                      color: Colors.teal,
-                    ),
-                    const Text(
-                      "Row2 Colors",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    Container(
-                      height: 50,
-                      width: 50,
-                      color: Colors.purple,
-                    ),
-                    Container(
-                      height: 50,
-                      width: 50,
-                      color: Colors.teal,
-                    ),
-                    Container(
-                      height: 50,
-                      width: 50,
-                      color: Colors.amber,
+                      margin: const EdgeInsets.only(left: 15),
+                      width: 100,
+                      height: 40,
+                      child: const Text(
+                        "Direction & Place",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                        ),
+                      ),
                     ),
                   ],
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                children: [
-                  Container(
-                    height: 50,
-                    width: 50,
-                    color: Colors.teal,
-                  ),
-                  Spacer(),
-                  Container(
-                    height: 50,
-                    width: 50,
-                    color: Colors.amber,
-                  ),
-                  Spacer(),
-                  Container(
-                    height: 50,
-                    width: 50,
-                    color: Colors.purple,
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              Row(
-                children: [
-                  Container(
-                    height: 170,
-                    width: 220,
-                    color: Colors.purple,
-                    margin: EdgeInsets.only(left: 20),
-                    child: Column(
-                      children: [
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Row(
-                          children: [
-                            Container(
-                              height: 75,
-                              width: 100,
-                              color: Colors.green,
-                              margin: EdgeInsets.only(left: 7),
-                            ),
-                            Container(
-                              height: 75,
-                              width: 100,
-                              color: Colors.blue,
-                            )
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Row(
-                          children: [
-                            Container(
-                              height: 75,
-                              width: 100,
-                              color: Colors.indigo,
-                              margin: EdgeInsets.only(left: 7),
-                            ),
-                            Container(
-                              height: 75,
-                              width: 100,
-                              color: Colors.tealAccent,
-                            )
-                          ],
-                        )
-                      ],
+                const Spacer(),
+                Column(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(left: 15),
+                      height: 60,
+                      width: 60,
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      child: const Icon(
+                        Icons.car_rental,
+                        size: 30,
+                        color: Colors.white,
+                      ),
                     ),
+                    const SizedBox(height: 10),
+                    Container(
+                      margin: const EdgeInsets.only(left: 15),
+                      width: 100,
+                      height: 40,
+                      child: const Text(
+                        "Transportation",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Image.asset(
+                    "assets/images/profile.jpeg",
+                    height: 200,
+                    width: 200,
+                  ),
+                  Spacer(),
+                  Image.asset(
+                    "assets/images/person1.jpeg",
+                    height: 200,
+                    width: 200,
+                  ),
+                  Spacer(),
+                  Image.asset(
+                    "assets/images/person2.jpeg",
+                    height: 200,
+                    width: 200,
                   ),
                 ],
-              )
-            ],
-          ),
+              ),
+            )
+          ],
         ),
       )),
     );
