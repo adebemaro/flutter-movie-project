2f7892c7aea4c040379aed2bf49a04d7d9695f80
adebemaro <adebemario@gmail.com>
2023-04-14 06:05:55 -0700

    home cleaning

--- lib/main.dart
+++ lib/main.dart
@@ -10,175 +10,103 @@ class MovieApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SafeArea(
+        debugShowCheckedModeBanner: false,
+        home: SafeArea(
           child: Scaffold(
-              backgroundColor: Colors.black26,
-              appBar: AppBar(
-                title: const Text("Learn German"),
-                actions: const [
-                  Padding(
-                    padding: EdgeInsets.only(right: 8.0),
-                    child: Icon(
-                      Icons.search,
-                      size: 30,
-                    ),
+            appBar: AppBar(
+              title: const Text("Learn German"),
+              actions: const [
+                Padding(
+                  padding: EdgeInsets.only(right: 8.0),
+                  child: Icon(
+                    Icons.search,
+                    size: 30,
                   ),
-                  Padding(
-                    padding: EdgeInsets.only(right: 8.0),
-                    child: CircleAvatar(
-                      backgroundColor: Colors.blueAccent,
-                      backgroundImage: AssetImage('assets/images/profile.jpeg'),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(right: 8.0),
+                  child: CircleAvatar(
+                    backgroundColor: Colors.blueAccent,
+                    backgroundImage: AssetImage('assets/images/profile.jpeg'),
+                  ),
+                )
+              ],
+              backgroundColor: Colors.grey,
+            ),
+            drawer: const Drawer(),
+            body: Column(
+              children: [
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.only(left: 10.0),
+                      child: Text(
+                        "Home Services",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
                     ),
-                  )
-                ],
-                backgroundColor: Colors.grey,
-              ),
-              drawer: const Drawer(),
-              body: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                    Spacer(),
+                    Padding(
+                      padding: EdgeInsets.only(right: 20.0),
+                      child: Text(
+                        'View All',
+                        style: TextStyle(
+                            color: Colors.orangeAccent,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
                     child: Row(
                       children: [
                         Column(
                           children: [
-                            const SizedBox(
-                              height: 10,
-                            ),
                             Container(
-                              height: 80,
-                              width: 100,
+                              height: 70,
+                              width: 70,
                               decoration: BoxDecoration(
-                                border:
-                                    Border.all(color: Colors.white, width: 2),
-                                borderRadius: BorderRadius.circular(25),
-                                color: Colors.amber,
-                                image: const DecorationImage(
-                                  image: AssetImage(
-                                    'assets/images/game_pic2.jpg',
-                                  ),
-                                  fit: BoxFit.fill,
-                                ),
+                                color: Color.fromARGB(255, 218, 218, 218),
+                                borderRadius: BorderRadius.circular(50),
                               ),
+                              child: Icon(Icons.plumbing),
                             ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            const Text(
-                              "Ballons TD 6",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            Row(
-                              children: const [
-                                Icon(
-                                  Icons.thumb_up,
-                                  color: Colors.green,
-                                  size: 18,
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.only(left: 8.0),
-                                  child: Text(
-                                    "100%",
-                                    style: TextStyle(color: Colors.green),
-                                  ),
-                                )
-                              ],
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            Container(
-                              width: 100,
-                              height: 20,
-                              decoration: BoxDecoration(
-                                  color: Colors.grey,
-                                  borderRadius: BorderRadius.circular(50)),
-                              child: const Center(
-                                child: Text(
-                                  "Free Shopping",
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                            )
+                            Text("Plumber"),
                           ],
                         ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 12.0),
-                          child: Column(
-                            children: [
-                              const SizedBox(
-                                height: 10,
+                        Column(
+                          children: [
+                            Container(
+                              height: 70,
+                              width: 70,
+                              decoration: BoxDecoration(
+                                color: Color.fromARGB(255, 218, 218, 218),
+                                borderRadius: BorderRadius.circular(50),
                               ),
-                              Container(
-                                height: 80,
-                                width: 100,
-                                decoration: BoxDecoration(
-                                  border:
-                                      Border.all(color: Colors.white, width: 2),
-                                  borderRadius: BorderRadius.circular(25),
-                                  color: Colors.amber,
-                                  image: const DecorationImage(
-                                    image: AssetImage(
-                                      'assets/images/game_pic1.jpg',
-                                    ),
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                              ),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              const Text(
-                                "Offroad CutLaw",
-                                style: TextStyle(color: Colors.white),
-                              ),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              Row(
-                                children: const [
-                                  Icon(
-                                    Icons.thumb_up,
-                                    color: Colors.green,
-                                    size: 18,
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.only(left: 8.0),
-                                    child: Text(
-                                      "100%",
-                                      style: TextStyle(color: Colors.green),
-                                    ),
-                                  )
-                                ],
-                              ),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              Container(
-                                width: 100,
-                                height: 20,
-                                decoration: BoxDecoration(
-                                    color: Colors.grey,
-                                    borderRadius: BorderRadius.circular(50)),
-                                child: const Center(
-                                  child: Text(
-                                    "Free Shopping",
-                                    style: TextStyle(color: Colors.white),
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
+                              child: Icon(Icons.plumbing),
+                            ),
+                            Text("Plumber"),
+                          ],
+                        )
                       ],
                     ),
                   ),
-                ],
-              ))),
-    );
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
